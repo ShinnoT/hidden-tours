ad711344c0173ece2a625b0af9605698247bde0d
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-03-21 11:59:45 +0900

    sample get route and concurrently to run both servers at once with one command

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("<h1>Express Server</h1><p>hello world :)</p>");
+});
+
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
