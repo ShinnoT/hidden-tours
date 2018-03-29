f46ade5a2624b35605a5d49002a6b718149056e9
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-03-29 23:34:25 +0900

    planid dynamic on plan component, accessed from backend server

--- client/src/components/Plan.js
+++ client/src/components/Plan.js
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Plan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plan: {}
+    };
+  }
+  componentDidMount() {
+    fetch(
+      `/tours/${this.props.match.params.tourId}/${
+        this.props.match.params.planId
+      }`
+    )
+      .then(res => res.json())
+      .then(plan => this.setState({ plan }));
+  }
   render() {
     return (
       <div className="Plan">
         <h1>Plan Page</h1>
+        <p>{this.state.plan.id}</p>
       </div>
     );
   }
